ee2823f4624576f899993176f1fc65203f26956b
NowrinIslamMim <nowrin02@gmail.com>
2022-08-22 16:27:23 +0600

    Error handled

--- js/app.js
+++ js/app.js
@@ -4,10 +4,42 @@
 function setNameOnTheList(id) {
     const playerNameElement = getPlayerName(id);
     const selectedPlayerField = document.getElementById('selected-players');
+
     const li = document.createElement('li');
     li.innerText = playerNameElement;
 
+    //show alert when more than five players selected
+    if (selectedPlayerField.childNodes.length >= 5) {
+        return alert('You can add more than Five!');
+    }
     selectedPlayerField.appendChild(li);
+
+
+    //selected button disable 
+    if (li.innerText === 'Lionel Messi') {
+        const button = document.getElementById('m-btn');
+        button.setAttribute('disabled', 'true');
+    }
+    else if (li.innerText === 'Cristiano Ronaldo') {
+        const button = document.getElementById('c-btn');
+        button.setAttribute('disabled', 'true');
+    }
+    else if (li.innerText === 'Kaká') {
+        const button = document.getElementById('k-btn');
+        button.setAttribute('disabled', 'true');
+    }
+    else if (li.innerText === 'Kylian Mbappé') {
+        const button = document.getElementById('mb-btn');
+        button.setAttribute('disabled', 'true');
+    }
+    else if (li.innerText === 'Neymar jr.') {
+        const button = document.getElementById('n-btn');
+        button.setAttribute('disabled', 'true');
+    }
+    else if (li.innerText === 'Ronaldinho') {
+        const button = document.getElementById('r-btn');
+        button.setAttribute('disabled', 'true');
+    }
 }
 
 //get the player name text by id
@@ -34,33 +66,39 @@ function setTextValueById(expenseId, newValue) {
     ExpenseElement.innerText = newValue;
 }
 
+//calculate the player expense
+function calculatePlayerExpense(playerFieldId) {
+    const perPlayerBudget = getBudgetById(playerFieldId);
+    const selectedPlayers = getSelectedPlayers('selected-players');
+    const playerExpenseTotal = perPlayerBudget * selectedPlayers;
+    return playerExpenseTotal;
+}
+//get player number from the list
+function getSelectedPlayers(selectedPlayersId) {
+    const selectedPlayersElement = document.getElementById(selectedPlayersId);
+    const selectedPlayers = selectedPlayersElement.childNodes.length;
+    return selectedPlayers;
+}
+
 document.getElementById('btn-calculate').addEventListener('click', function () {
 
-    //get player number from the list
-    const selectedPlayersElement = document.getElementById('selected-players');
-    const selectedPlayers = selectedPlayersElement.childNodes.length;
-
-    //calculate the player expense
-    const perPlayerBudget = getBudgetById('player-field');
-    const playerExpenseTotal = perPlayerBudget * selectedPlayers;
-
+    //get the player Expense
+    const playerExpenseTotal = calculatePlayerExpense('player-field');
+    //set the calculated expense
     setTextValueById('player-expense', playerExpenseTotal);
 
 })
 
 document.getElementById('btn-calculate-total').addEventListener('click', function () {
 
-    const selectedPlayersElement = document.getElementById('selected-players');
-    const selectedPlayers = selectedPlayersElement.childNodes.length;
-
-    const perPlayerBudget = getBudgetById('player-field');
-    const playerExpenseTotal = perPlayerBudget * selectedPlayers;
-
+    //get The Expenses by function
+    const playerExpenseTotal = calculatePlayerExpense('player-field');
     const managerExpense = getBudgetById('manager-field');
     const coachExpense = getBudgetById('coach-field');
 
-
+    //calculate the total expense
     const totalExpenses = playerExpenseTotal + managerExpense + coachExpense;
 
+    //set the total expenses value
     setTextValueById('total-expense', totalExpenses);
 })
